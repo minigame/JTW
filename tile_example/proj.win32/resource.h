2fc2f86ad0f20d89c15569ff8eb3a62502892397
Fe2plus-Raymond <Fe2plus-Raymond@users.noreply.github.com>
2014-08-16 23:41:19 +0800

    Add tile_example

---
+++ tile_example/proj.win32/resource.h
@@ -0,0 +1,27 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 game.rc 使用
+//
+#define IDS_PROJNAME                    100
+#define IDR_TESTJS                      100
+#define IDR_ACCELERATOR1                201
+#define IDR_CAF1                        202
+#define IDB_PNG1                        203
+#define IDR_CAF2                        204
+#define IDR_CAF3                        205
+#define IDB_PNG2                        206
+#define IDR_CAF4                        207
+#define IDR_TMX1                        208
+#define IDB_PNG3                        209
+#define ID_FILE_NEW_WINDOW              32771
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        210
+#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
