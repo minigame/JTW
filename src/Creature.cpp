9edbfcd524b867c5f5123d7ce8e14a582ab25f99
llchaha <liulunchun@163.com>
2014-08-16 14:38:55 +0800

    生物类

--- src/Creature.cpp
+++ src/Creature.cpp
@@ -2,23 +2,25 @@
 
 Creature::Creature()
 {
-	m_speed = Vec2(0.0f, 0.0f);
-	m_acceleration = Vec2(0.0f, 0.0f);
 	m_Position = Vec2(0.0f, 0.0f);
 	m_CurrentBlood = 0.0f;
 	m_MaxBlood = 0.0f;
+	m_dir = Right;
 }
 
 Creature::~Creature()
 {
 	//这块是不是要释放精灵动画
+	if(m_armature)
+	{
+		delete m_armature;
+		m_armature = NULL;
+	}
 }
 
-Creature::Creature(Vec2 speed, Vec2 acceleration, Vec2 pos, float currentBlood, float maxBlood)
+Creature::Creature(float currentBlood, float maxBlood, DIR d)
 {
-	m_speed = speed;
-	m_acceleration = acceleration;
-	m_Position = pos;
+	
 	//m_vPosition = pos;
 	if(currentBlood >= 0.0f)
 		m_CurrentBlood = currentBlood;
@@ -30,34 +32,114 @@ Creature::Creature(Vec2 speed, Vec2 acceleration, Vec2 pos, float currentBlood, 
 	else
 		m_MaxBlood = 0.0f;
 
+	m_dir = d;
 	//m_bInScene = bs;
 }
 
-void Creature::updatePosition(float dt)
+//void Creature::updatePosition(float dt)
+//{
+//	///////////////////这块是不是要判断位置的上下限；
+//	//m_vPosition = m_vPosition + m_vXSpeed*dt + m_vYSpeed*dt + Vec2(0.0f, 0.5f*dt*dt);
+//	m_Position = calcPosition(dt);
+//}
+//
+//void Creature::updateSpeed(float dt)
+//{
+//	//m_vYSpeed = m_vYSpeed + Vec2(0, m_fGravityAcceleration*dt); 
+//	m_speed = m_speed + m_acceleration*dt;
+//}
+//
+//
+//void Creature::setPosition(Vec2 pos)
+//{
+//	if(pos != m_Position)
+//	{
+//		m_Position = pos;
+//	}
+//}
+//
+//Vec2 Creature::calcPosition(float t)
+//{
+//	Vec2 pos;
+//	pos = m_Position + m_speed*t + 0.5f*m_acceleration*t*t;
+//	return pos;
+//}
+
+void Creature::setArmature(cocostudio::Armature* armature)
 {
-	///////////////////这块是不是要判断位置的上下限；
-	//m_vPosition = m_vPosition + m_vXSpeed*dt + m_vYSpeed*dt + Vec2(0.0f, 0.5f*dt*dt);
-	m_Position = calcPosition(dt);
+	if(m_armature != armature)
+		m_armature = armature;
 }
 
-void Creature::updateSpeed(float dt)
+
+void Creature::setPhyBody(cocos2d::PhysicsBody* bodyBox)
 {
-	//m_vYSpeed = m_vYSpeed + Vec2(0, m_fGravityAcceleration*dt); 
-	m_speed = m_speed + m_acceleration*dt;
+	if(m_phyBox != bodyBox)
+		m_phyBox = bodyBox;
+}
+
+void Creature::bindPhyBody()
+{
+	if(m_phyBox && m_armature)
+		m_armature->setPhysicsBody(m_phyBox);
 }
 
 
-void Creature::setPosition(Vec2 pos)
+void Creature::setTag(int tag)
 {
-	if(pos != m_Position)
+	if(m_armature)
+		m_armature->setTag(tag);
+}
+
+
+void Creature::setArmatureWithAnimationName(char* name)
+{
+	if(m_armature)
 	{
-		m_Position = pos;
+		delete m_armature;
+		m_armature = NULL;
+	}
+	m_armature = cocostudio::Armature::create(name);
+}
+
+void Creature::setArmatureWithExportJsonFile(char* filename, char* armatureName)
+{
+	if(m_armature)
+	{
+		delete m_armature;
+		m_armature = NULL;
+	}
+	cocostudio::ArmatureDataManager::getInstance()->addArmatureFileInfo(filename);
+	m_armature = cocostudio::Armature::create(armatureName);
+}
+
+///////////这个没有写好
+void Creature::setAnimationDirByDIR()
+{
+	if(m_dir == Right)   //正常播放行走动画
+	{
+
+	}
+	else    //该生物现在向左行走，所以他的动画需要反向播放
+	{
+		//m_armature->setr
+		//m_armature->setRotationSkewY
+		//m_armature->setRotationY();
 	}
 }
 
-Vec2 Creature::calcPosition(float t)
+void Creature::setDir(DIR d)
 {
-	Vec2 pos;
-	pos = m_Position + m_speed*t + 0.5f*m_acceleration*t*t;
-	return pos;
+	m_dir = d;
 }
+
+void Creature::setSpeed(Vec2 v)
+{
+	if(m_armature && m_phyBox)
+	{
+		m_armature->getPhysicsBody()->setVelocity(v);
+		//m_phyBox->setVelocity();
+	}
+}
+
+
--- src/Creature.h
+++ src/Creature.h
@@ -16,10 +16,11 @@
 */
 
 #include "cocos2d.h"
+#include "cocostudio\CocoStudio.h"
 
 using namespace cocos2d;
 
-
+enum DIR {Left, Right};   //该Creature的方向
 
 class Creature
 {
@@ -31,29 +32,41 @@ public:
 
 //成员变量
 public:
-	Vec2 m_speed;    //速度，第一个参数是x轴方向速度，第二个参数是Y轴方向速度
-	Vec2 m_acceleration;   //加速度   第一个参数是x轴的加速度，第二个参数是Y轴的加速度（一般是重力）
+	//Vec2 m_speed;    //速度，第一个参数是x轴方向速度，第二个参数是Y轴方向速度
+	//Vec2 m_acceleration;   //加速度   第一个参数是x轴的加速度，第二个参数是Y轴的加速度（一般是重力）
 	/**************************************************生物的精灵图片或动画，player和NPC都包括进来了
 	Sprite* m_pSprite;
 	Animation* m_pAnimation;
 	Animate* m_pAnimate;
 	**************************************************/
-	Vec2 m_Position;     //改生物的位置，这个位置是相对于卷轴的,且是中心位置
+	Vec2 m_Position;     //改生物的位置，这个位置是相对于卷轴的,且是中心位置    ??????????????????
 	float m_CurrentBlood;     //血量
 	float m_MaxBlood;        //该生物最大血量值
-	/*bool m_bInScene; */   //改生物时候在场景
+	cocostudio::Armature* m_armature;    //该生物的armature
+	cocos2d::PhysicsBody* m_phyBox;
+	DIR m_dir;    //该Creature的方向
 
 //构造函数
 public:
-	Creature(Vec2 speed, Vec2 acceleration, Vec2 pos, float currentBlood, float maxBlood);
+	//Creature(Vec2 speed, Vec2 acceleration, Vec2 pos, float currentBlood, float maxBlood);
+	Creature(float currentBlood, float maxBlood, DIR d);
 
 //成员函数
 public:
 	virtual void init(){}    //初始化函数，生物的精灵图片或动画的生成写在这
-	void updatePosition(float dt);     //更新该生物的位置
-	void updateSpeed(float dt);     //更新Y轴的速度
-	void setPosition(Vec2 pos);   //设置生物位置
-	Vec2 calcPosition(float t);      //计算经过一段时间t，该生物的位置
+	void setArmature(cocostudio::Armature* armature);    //设置该生物的armature
+	void setPhyBody(cocos2d::PhysicsBody* bodyBox);     //设置该生物的物理属性body这个成语变量，不代表该armature和body绑定
+	void bindPhyBody();    //绑定armature和body
+	void setTag(int tag);   //给m_armature设置tag
+	void setArmatureWithAnimationName(char* name);   //从ArmatureDataManagerChe里面通过动画名字，给Armature赋值
+	void setArmatureWithExportJsonFile(char* filename, char* armatureName);    //通过ExportJson的文件名和动画名字创建Armature给m_armature
+	void setAnimationDirByDIR();      //通过该生物的DIR设置动画播放的方向，向右走是正常播放，向左走是反向播放
+	void setDir(DIR d);    //设置该生物的行走方向
+	void setSpeed(Vec2 v);    //设置该生物的物理速度
+	//void updatePosition(float dt);     //更新该生物的位置
+	//void updateSpeed(float dt);     //更新Y轴的速度
+	//void setPosition(Vec2 pos);   //设置生物位置
+	//Vec2 calcPosition(float t);      //计算经过一段时间t，该生物的位置
 };
 
 #endif
