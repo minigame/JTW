206327f6150f4e766b4b34e647485a1817643529
alexzzp <alexzzp@126.com>
2014-08-19 21:06:01 +0800

    删除与flappy bird相关的资源

--- src/WelcomeLayer.cpp
+++ src/WelcomeLayer.cpp
@@ -22,13 +22,13 @@ bool WelcomeLayer::init()
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
+
 	Sprite *sprite = Sprite::createWithSpriteFrame(ResourceMgr::getInstance()->getImage("GameTitle"));
-
 	sprite->setPosition(Vec2(origin.x + visibleSize.width/2 ,origin.y + visibleSize.height*0.618 ));
-	
-
 	cocostudio::Armature * armature = cocostudio::Armature::create("monkey_jump");
 	armature->getAnimation()->playWithIndex(0);
+	
+
 	//armature->setPosition(Vec2(origin.x + visibleSize.width / 2 - sprite->getContentSize().width / 2,
 	//	origin.y + visibleSize.height*0.4 - sprite->getContentSize().height / 2));
 	sprite->addChild(armature);
