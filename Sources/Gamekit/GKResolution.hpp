6eddb9e50e7552cb1ce28500a0c0a1851c1876b1
coreyzhu <coreyzhu@tencent.com>
2014-11-15 12:05:27 +0800

    加入设备分辨率

---
+++ Sources/Gamekit/GKResolution.hpp
@@ -0,0 +1,35 @@
+/*
+ *  Copyright(c), All rights reserved.
+ *  Author:          alexzzp@gmail.com
+ *  Filename:        GKResolution.hpp
+ *  Created Time:    2014/11/15  11:57:53
+ *  Description:     包含各种设备的分辨率
+ *  Revision:
+ *  License:
+ */
+
+/* ---------- phone ---------- */
+
+#define GK_RESOLUTION_IPHONE_4_WIDTH      (320)
+#define GK_RESOLUTION_IPHONE_4_HEIGHT     (480)
+
+#define GK_RESOLUTION_IPHONE_5_WIDTH      (320)
+#define GK_RESOLUTION_IPHONE_5_HEIGHT     (568)
+
+#define GK_RESOLUTION_IPHONE_6_WIDTH      (375)
+#define GK_RESOLUTION_IPHONE_6_HEIGHT     (667)
+
+#define GK_RESOLUTION_IPHONE_6P_WIDTH     (414)
+#define GK_RESOLUTION_IPHONE_6P_HEIGHT    (736)
+
+#define GK_RESOLUTION_PHONE_REDMI_WIDTH   (1280)
+#define GK_RESOLUTION_PHONE_REDMI_HEIGHT  (720)
+
+/* ---------- ipad ---------- */
+
+#define GK_RESOLUTION_IPAD_WIDTH          (768)
+#define GK_RESOLUTION_IPAD_HEIGHT         (1024)
+
+/* ---------- pc/mac ---------- */
+
+/* ---------- misc ---------- */
