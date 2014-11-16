f5687198110ff859e1e1f57ad3b3e6daf23ea892
alexzzp <alexzzp@gmail.com>
2014-11-17 00:50:53 +0800

    开始修改ios的工程，先确保原来的jtw能够顺利在ios上编译运行

--- proj.ios_mac/ios/main.m
+++ proj.ios_mac/ios/main.m
@@ -1,6 +1,25 @@
 #import <UIKit/UIKit.h>
 
+#include "cocos2d.h"
+// #include "AppDelegate.h"
+#include "Instance.h"
+
+#include "ResourceLoader.h"
+#include "ResourceMgr.h"
+#include "CallBackMgr.h"
+#include "DiCiData.h"
+#include <vector>
+
 int main(int argc, char *argv[]) {
+    
+    //≥ı ºªØ◊ ‘¥º”‘ÿµ•¿˝
+    ResourceLoader * ResourceLoader::m_instance = new ResourceLoader();
+    //≥ı ºªØ◊ ‘¥π‹¿Ì¿‡
+    ResourceMgr * ResourceMgr::m_instance = new ResourceMgr();
+    
+    CallBackMgr * CallBackMgr::m_instance = new CallBackMgr();
+    
+    std::vector<DiCiData*>* diciVector = new std::vector<DiCiData*>();
     
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
