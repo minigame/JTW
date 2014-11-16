d1b84ce8dd54eee25b6dc9a9ae42eb1c6466a42f
alexzzp <alexzzp@gmail.com>
2014-11-16 12:00:05 +0800

    加入瓦片地图加载类

---
+++ Sources/JTWTileMapLoader.cpp
@@ -0,0 +1,42 @@
+/*
+ *  Copyright(c), All rights reserved.
+ *  Author:          alexzzp@gmail.com
+ *  Filename:        JTWTileMapLoader.cpp
+ *  Created Time:    2014/11/16  11:50:47
+ *  Description:     游戏地图加载器
+ *  Revision:
+ *  License:
+ */
+
+#include "JTWTileMapLoader.hpp"
+
+USING_NS_CC;
+
+/* -------------------------------------------------- */
+/* cocso2d framework */
+
+JTWTileMapLoader::JTWTileMapLoader()
+{
+
+}
+
+JTWTileMapLoader::~JTWTileMapLoader()
+{
+
+}
+
+/* -------------------------------------------------- */
+/* application logic */
+
+
+int JTWTileMapLoader::loadTileMap()
+{
+    
+
+
+
+    return 0;
+}
+
+
+
